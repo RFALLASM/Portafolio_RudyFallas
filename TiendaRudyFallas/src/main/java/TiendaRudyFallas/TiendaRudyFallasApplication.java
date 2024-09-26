21a839d350935750910fef5eadbe251b78ccdb02
RFALLASM <rfallas50641@ufide.ac.cr>
2024-09-25 21:05:15 -0500

    Creación de Proyecto
    Se creo el proyecto desde 0

---
+++ TiendaRudyFallas/src/main/java/TiendaRudyFallas/TiendaRudyFallasApplication.java
@@ -0,0 +1,13 @@
+package TiendaRudyFallas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaRudyFallasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaRudyFallasApplication.class, args);
+	}
+
+}
---
+++ TiendaRudyFallas/src/test/java/TiendaRudyFallas/TiendaRudyFallasApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaRudyFallas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaRudyFallasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
